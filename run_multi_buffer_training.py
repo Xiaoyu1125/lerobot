5d84cceaaca581e1fdf0139e569c3412fa8f5dd8
Xiaoyu Jiang <xiaoyu@alfa.iiis.io>
2026-03-11 13:53:36 +0800

    bash for vlm model

--- run_multi_buffer_training.py
+++ run_multi_buffer_training.py
@@ -22,7 +22,8 @@ def run_training():
         # "--dataset.repo_id=/public/xiaoyu/svla_so100_stacking",
         "--batch_size=32",
         "--steps=200000",
-        "--policy.repo_id=/home/git/lerobot/smolvla_base",
+        "--policy.repo_id=/data/models",
+        "--policy.vlm_model_name=/data/models"
         # "--policy.repo_id=/home/xiaoyu/lerobot/lerobot/outputs",
         "--wandb.enable=true",
         "--wandb.mode=online",
